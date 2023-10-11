a94002673284adbb2a240beec243d9df7e20a609
GreatGaspargoo <b.c.young.97@gmail.com>
2023-10-11 08:39:41 -0600

    edited typo, console logs for debug

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,10 @@
 const withAuth = (req, res, next) => {
+  console.log('Entering withAuth middleware');
   // If the user is not logged in, redirect the user to the login page
-  if (!req.session.loggedIn) {
+  if (!req.session.logged_in) {
     res.redirect('/login');
   } else {
+    console.log('Exiting withAuth middleware');
     next();
   }
 };
